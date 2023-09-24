7882d8ea4b77f9c74eded4321d4fc8c31bbfb8d1
Vadim Gorbachev <vadim.gorbachev.dev@gmail.com>
2023-09-25 04:29:21 +0800

    add Makefile, change logger format to json

---
+++ extractor/src/config.rs
@@ -0,0 +1,53 @@
+extern crate dotenv;
+
+use log;
+use std::env;
+use envconfig::Envconfig;
+
+#[derive(Debug, Envconfig)]
+pub struct ConfigRabbitMQ {
+    #[envconfig(from = "RABBITMQ_ADMIN_USER")]
+    pub user: String,
+    #[envconfig(from = "RABBITMQ_ADMIN_PASSWORD")]
+    pub password: String,
+    #[envconfig(from = "RABBITMQ_HOST")]
+    pub host: String,
+    #[envconfig(from = "RABBITMQ_VHOST")]
+    pub vhost: String,
+    #[envconfig(from = "RABBITMQ_PORT")]
+    pub port: u16,
+}
+
+impl ConfigRabbitMQ {
+    pub fn get_url(&self) -> String {
+        format!(
+            "amqp://{}:{}@{}:{}",
+            self.user, self.password, self.host, self.port
+        )
+    }
+}
+
+#[derive(Debug, Envconfig)]
+pub struct Config {
+    #[envconfig(nested = true)]
+    pub rabbit: ConfigRabbitMQ,
+    #[envconfig(from = "LOG_LEVEL")]
+    pub log_level: String,
+}
+
+pub fn get() -> Config {
+    // If LOCAL_RUN is set, load .env file
+    if !env::var("LOCAL_RUN").is_err() {
+        use dotenv::dotenv;
+
+        dotenv().ok();
+        log::info!("Local Run mode enabled")
+    }
+
+    let config = match Config::init_from_env() {
+        Ok(config) => config,
+        Err(e) => panic!("Error loading config: {}", e),
+    };
+    log::debug!("rabbit url: {}", config.rabbit.get_url());
+    config
+}
--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -1,9 +1,19 @@
+extern crate log;
+
 use crate::extractor::Extractor;
 use std::collections::HashMap;
 
+
 mod extractor;
+mod config;
 
 fn main() {
+    json_env_logger::init();
+
+
+    log::info!("starting extractor");
+    let conf = config::get();
+    
     let data = r##"
     <HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">
         <TITLE>301 Moved</TITLE></HEAD><BODY>
@@ -19,5 +29,5 @@ fn main() {
     ]);
     let xpath_extractor = extractor::XpathExtractor::new(data, exprs);
     let res = xpath_extractor.extract();
-    println!("{:?}", res);
+    log::info!("res: {:?}", res);
 }
